8b4d484647d8625d0e77510209b72fc1003beb1d
Madhav <madhav@pop-os.localdomain>
2020-03-17 21:55:04 +0530

    added links

--- app.py
+++ app.py
@@ -3,17 +3,22 @@ import pandas as pd
 from flask import Flask, render_template, Response, request, redirect, url_for,Markup,request
 conn = sqlite3.connect('cv.db')  
 c = conn.cursor()
+c.execute('''SELECT * FROM cv_posts''')
+query=c.fetchall()
+df = pd.DataFrame(query, columns=['Id','PostTypeId','AcceptedAnswerId','ParentId','CreationDate','Score','ViewCount','Body','OwnerUserId','OwnerDisplayName','LastEditorUserId','LastEditDate','LastActivityDate','Title','Tags','AnswerCount','CommentCount','ClosedDate','FavoriteCount'])
+body_dict={}
+for i in range(len(query)):
+   body_dict[i]=df['Body'][i]
+print(len(body_dict),len(df['Body'][0]))
+conn = sqlite3.connect('cvlinks.db')  
+c = conn.cursor()
 c.execute('''SELECT * FROM cv_posts ORDER BY CreationDate DESC LIMIT 10''')
 query=c.fetchall()
 def get_table_html(query):
    if not query:
       return("No matches found")
    df = pd.DataFrame(query, columns=['Id','PostTypeId','AcceptedAnswerId','ParentId','CreationDate','Score','ViewCount','Body','OwnerUserId','OwnerDisplayName','LastEditorUserId','LastEditDate','LastActivityDate','Title','Tags','AnswerCount','CommentCount','ClosedDate','FavoriteCount'])
-   dict=df.to_dict()
-   dict['Body'][0]=dict['Body'][0].replace("&lt;","<")
-   dict['Body'][0]=dict['Body'][0].replace("&gt;",">")
-   df=pd.DataFrame(dict)
-   # print(df['Body'][0])
+   
    html=df.to_html(classes="table table-striped")
    html=html.replace("&lt;","<")
    html=html.replace("&gt;",">")
@@ -28,13 +33,13 @@ def index():
 
 @app.route("/ViewCount/", methods=['POST'])
 def sort_veiw():
-   conn = sqlite3.connect('cv.db')  
+   conn = sqlite3.connect('cvlinks.db')  
    c = conn.cursor()
    c.execute('''SELECT * FROM cv_posts ORDER BY ViewCount DESC''')
    return render_template('index.html', table=Markup(get_table_html(c.fetchall())))
 @app.route("/score/", methods=['POST'])
 def sort_score():
-   conn = sqlite3.connect('cv.db')  
+   conn = sqlite3.connect('cvlinks.db')  
    c = conn.cursor()
    c.execute('''SELECT * FROM cv_posts ORDER BY Score DESC''')
    return render_template('index.html', table=Markup(get_table_html(c.fetchall())))
@@ -49,5 +54,8 @@ def search():
    c.execute(kp)
    # print(c.fetchall())
    return render_template('index.html', table=Markup(get_table_html(c.fetchall())))
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+   return body_dict[int(post_id)]
 if __name__ == "__main__":
    app.run()
--- create_db.py
+++ create_db.py
@@ -2,12 +2,20 @@ import sqlite3
 import pandas as pd
 from flask import url_for
 data=pd.read_csv('cv_posts.csv').to_dict()
+data_links=pd.read_csv('cv_posts.csv').to_dict()
 for i in range(len(data['CreationDate'])):
     data['Body'][i]=str(data['Body'][i]).replace("\\n","<br />")
     data['Body'][i]=str(data['Body'][i]).replace("&lt;-","<")
     data['Body'][i]=str(data['Body'][i]).replace("&gt;",">")
+    data_links['Body'][i]=f"<a href='/post/{i}'>open    </a>"
 df=pd.DataFrame.from_dict(data)
+df_links=pd.DataFrame.from_dict(data_links)
 conn = sqlite3.connect('cv.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE 'cv_posts' ([id] INTEGER,[PostTypeId] INTEGER,[AcceptedAnswerId] INTEGER,[ParentId] INTEGER,[CreationDate] datetime,[Score] INTEGER,[ViewCount] INTEGER,'Body',[OwnerUserId] INTEGER,[OwnerDisplayName] INTEGER,[LastEditorUserId] INTEGER,[LastEditDate] datetime,[LastActivityDate] datetime,'Title','Tags',[AnswerCount] INTEGER,[CommentCount] INTEGER,[ClosedDate] datetime,[FavoriteCount] datetime)''')
 df.to_sql('cv_posts', conn, if_exists='append', index = False)
+conn.close()
+conn = sqlite3.connect('cvlinks.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE 'cv_posts' ([id] INTEGER,[PostTypeId] INTEGER,[AcceptedAnswerId] INTEGER,[ParentId] INTEGER,[CreationDate] datetime,[Score] INTEGER,[ViewCount] INTEGER,'Body',[OwnerUserId] INTEGER,[OwnerDisplayName] INTEGER,[LastEditorUserId] INTEGER,[LastEditDate] datetime,[LastActivityDate] datetime,'Title','Tags',[AnswerCount] INTEGER,[CommentCount] INTEGER,[ClosedDate] datetime,[FavoriteCount] datetime)''')
+df_links.to_sql('cv_posts', conn, if_exists='append', index = False)
